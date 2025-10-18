183addf99719e41a566941293c1b98ed5962cf0b
Delthoid <delthonvillanueva20@gmail.com>
2025-10-18 17:09:49 +0800

    #6 Setup API config

---
+++ lib/config/api_config.dart
@@ -0,0 +1,5 @@
+class ApiConfig {
+  static const String _baseUrl = 'http://localhost:8080/';
+
+  static String get baseUrl => _baseUrl;
+}
---
+++ lib/config/api_endpoints.dart
@@ -0,0 +1,14 @@
+class ApiEndpoints {
+  // Endpoints
+  // Auth
+  static const String _authBase = 'auth/';
+  static const String login = '$_authBase/login';
+  static const String signUp = '$_authBase/signup';
+
+  // Server
+  static const String _serverBase = 'server/';
+  static const String createServer = '$_serverBase/create';
+  static const String updateServer = '$_serverBase/update';
+  static const String deleteServer = '$_serverBase/delete';
+  static const String getServers = _serverBase;
+} 
